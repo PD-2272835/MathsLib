c6591b0b021432791e874336ed0337cc5afdaeb9
PD-2272835 <186196474+PD-2272835@users.noreply.github.com>
2026-04-22 13:13:23 +0100

    Add Matrix ToString Static Method

--- functions.hpp
+++ functions.hpp
@@ -3,6 +3,7 @@
 
 #include "constants.hpp"
 #include "vec.hpp"
+#include "matrix.hpp"
 #include <string>
 
 namespace mfg
@@ -29,6 +30,23 @@ namespace mfg
 		for (std::size_t i = 0; i < dim; ++i)
 		{
 			res += std::to_string(i) + " " + std::to_string(vector.values[i]) + " ";
+		}
+		return res;
+	}
+
+	template<std::size_t R, std::size_t C, typename T>
+	static std::string MatToString(const mfg::mat<R, C, T>& matrix)
+	{
+		std::string res;
+		for (std::size_t i = 0; i < R; ++i)
+		{
+			std::string line = "( ";
+			for (std::size_t j = 0; j < C; ++j)
+			{
+				line += std::to_string(matrix.values[(j*R)+i]) + ", ";
+			}
+			line += ")\n";
+			res += line;
 		}
 		return res;
 	}
